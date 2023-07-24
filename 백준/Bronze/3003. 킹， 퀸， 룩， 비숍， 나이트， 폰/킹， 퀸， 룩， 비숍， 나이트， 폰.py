06a721b051f551f9daf2c53ee30769a5af1a2928
manjookim <106953846+manjookim@users.noreply.github.com>
2023-07-24 23:57:59 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,11 @@
+arr = []
+a, b, c, d, e, f = map(int,input().split())
+arr.append(1-a)
+arr.append(1-b)
+arr.append(2-c)
+arr.append(2-d)
+arr.append(2-e)
+arr.append(8-f)
+    
+for each in arr:
+    print(each, end= ' ')
