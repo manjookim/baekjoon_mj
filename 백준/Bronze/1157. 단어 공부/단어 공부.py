f7066e2d6ce6a3fa0714c1037fbb412c02ec5a37
manjookim <106953846+manjookim@users.noreply.github.com>
2023-07-27 01:52:35 +0900

    [Bronze I] Title: 단어 공부, Time: 432 ms, Memory: 40044 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,8 @@
+word = list(input().upper())
+arr = []
+for i in range(ord("A"),ord("Z")+1):
+    arr.append(word.count(chr(i)))
+if arr.count(max(arr)) != 1 :
+    print("?")
+else:
+    print(chr(arr.index(max(arr))+65))
