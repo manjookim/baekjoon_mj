6724897778e3afd165058a86b164f45b82bc9baa
manjookim <106953846+manjookim@users.noreply.github.com>
2024-08-29 15:33:40 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,14 @@
+int main(){
+    int s;
+    scanf("%d", &s);
+    if (s>=90)
+        printf("A");
+    else if (s>=80)
+        printf("B");
+    else if (s>=70)
+        printf("C");
+    else if (s>=60)
+        printf("D");
+    else
+        printf("F");
+}
